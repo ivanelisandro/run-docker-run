5801dc811f0f0d0ac67ec5e4fd27eceb7c8b6dbd
Ivan Silva <ivan.elisandro@gmail.com>
2025-06-13 02:47:03 -0300

    stage 5: clean up after finishing the project
    this stage does not change the project because it is only about
    removing the container and images we will no longer use
    
    important files:
    - ./Run Docker Run/task/hyper-app/stage5.md

---
+++ Run Docker Run/task/hyper-app/stage5.md
@@ -0,0 +1,9 @@
+# Stage 5:
+
+In this stage is about cleaning up the environment after delivering finishing the project.
+
+## Objectives
+
+- Stop the container which has the image `hyper-web-app`;
+- Delete the container that the image is `hyper-web-app`; 
+- Remove the image with the tag `hyper-web-app:latest`.
--- Run Docker Run/task/test/tests.py
+++ Run Docker Run/task/test/tests.py
@@ -1,13 +1,8 @@
 import docker
-import requests
 from hstest import StageTest, dynamic_test, CheckResult
 
 ancestor = "hyper-web-app"
-url = "http://localhost:8000"
-server_response = {
-    "first_name": "Hyper",
-    "id": "90", "last_name": "Skill"
-}
+project_images = ["hyper-web-app"]
 
 class DockerTest(StageTest):
 
@@ -22,7 +17,7 @@ class DockerTest(StageTest):
 
     @dynamic_test()
     def test1(self):
-        """Tests image of container, state and exposed port"""
+        """Tests if the container is deleted"""
         if self.client is None:
             return CheckResult.wrong("Docker client is not initialized. Is Docker installed and running?")
         try:
@@ -32,36 +27,24 @@ class DockerTest(StageTest):
 
         for container in all_containers:
             if container.attrs.get("Config").get("Image") == ancestor:
-                if container.attrs.get("State").get("Running"):
-                    if "8000" in str(container.attrs.get("Config").get("ExposedPorts").keys()):
-                        return CheckResult.correct()
-                    else:
-                        return CheckResult.wrong("The exposed port should be 8000!")
-                else:
-                    return CheckResult.wrong("The container should be running!")
+                return CheckResult.wrong(f"You should delete the container for the ancestor '{ancestor}'!")
 
-        return CheckResult.wrong(f"Couldn't find a container from the '{ancestor}' ancestor!")
+        return CheckResult.correct()
 
     @dynamic_test()
     def test2(self):
-        """Tests if the webserver is running as expected"""
+        """Tests if the image is removed from the system"""
+        if self.client is None:
+            return CheckResult.wrong("Docker client is not initialized. Is Docker installed and running?")
         try:
-            response = requests.get(url)
-        except requests.exceptions.ConnectionError:
-            return CheckResult.wrong("Connection error!")
-        except requests.exceptions.InvalidURL:
-            return CheckResult.wrong("Couldn't connect to the server. Check if hyper-web-app is running as expected!")
-        except requests.exceptions.Timeout:
-            return CheckResult.wrong("Connection timed out, while trying to connect hyper-web-app!")
-        else:
-            if response.status_code != 200:
-                return CheckResult.wrong("Wrong status code returned from the hyper-web-app!")
-            try:
-                json_response = response.json()
-            except requests.exceptions.JSONDecodeError:
-                return CheckResult.wrong("Couldn't convert server response to dictionary!")
-            if json_response != server_response:
-                return CheckResult.wrong("Wrong content returned from the hyper-web-app!")
+            images_text = " ".join([str(image) for image in self.client.images.list()])
+        except docker.errors.DockerException as e:
+            return CheckResult.wrong(f"Cannot connect to the Docker daemon: {e}")
+
+        for image in project_images:
+            if image in images_text:
+                return CheckResult.wrong(f"You should delete the image '{image}'!")
+
         return CheckResult.correct()
 
 if __name__ == '__main__':
